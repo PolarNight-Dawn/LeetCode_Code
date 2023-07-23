6c8136cf7e5f348b066a905e3a86cd39b0e504df
polarnight <1813959165@qq.com>
2023-07-23 16:11:16 +0800

    added 47_permuteUnique.cpp's algorithm

--- 41-50/47_permuteUnique.cpp
+++ 41-50/47_permuteUnique.cpp
@@ -3,44 +3,49 @@
 #include <algorithm>
 
 //
-// Created by polarnight on 23-7-22.
+// Created by polarnight on 23-7-23.
 //
 
 class Solution {
 public:
-    void backTracing(std::vector<int> &nums, std::vector<int> &temp,
+    void backTracing(std::vector<int> &nums, std::vector<int> &temp, std::vector<int> &used,
                      std::vector<std::vector<int>> &res, int len) {
         if (temp.size() == len) {
             res.push_back(temp);
             return;
         }
 
-        for (int i = 0; i < len; i++) {
-            if(std::find(temp.begin(), temp.end(), nums[i]) == temp.end()) {
+        for(int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1] == 0) continue;
+            if (!used[i]) {
+                used[i] = 1;
                 temp.push_back(nums[i]);
-                backTracing(nums, temp, res, len);
+                backTracing(nums, temp, used, res, len);
                 temp.pop_back();
+                used[i] = 0;
             }
         }
     }
 
-    std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+    std::vector<std::vector<int>> permuteUnique(std::vector<int> &nums) {
         int len = nums.size();
         std::vector<int> temp;
+        std::vector<int> used(nums.size(), 0);
         std::vector<std::vector<int>> res;
 
-        backTracing(nums, temp, res, len);
+        std::sort(nums.begin(), nums.end());
+        backTracing(nums, temp, used, res, len);
 
         return res;
     }
 };
 
-int main46() {
-    std::vector<int> nums = {1, 2, 3};
+int main() {
+    std::vector<int> nums = {1, 1, 2};
     std::vector<std::vector<int>> res;
 
     Solution sol;
-    res = sol.permute(nums);
+    res = sol.permuteUnique(nums);
 
     for (int i = 0; i < res.size(); i++) {
         for (int j = 0; j < res[i].size(); j++) {
