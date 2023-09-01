212d8ed3b9f16e5afc05f757ecc548329d4499f7
polarnight <1813959165@qq.com>
2023-09-01 14:16:36 +0800

    added 111_minDepth.cpp's algorithm

--- 111-120/111_minDepth.cpp
+++ 111-120/111_minDepth.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 
 //
 // Created by polarnight on 23-9-1.
@@ -21,21 +20,26 @@ struct TreeNode {
 
 class Solution {
 public:
-    int get_height(TreeNode *root) {
+    int find_height02(TreeNode *root) {
         if (root == nullptr) return 0;
-        int left = get_height(root->left);
-        if (left == -1) return -1;
-        int right = get_height(root->right);
-        if (right == -1 || abs(left - right) > 1) return -1;
-        return std::max(left, right) + 1;
+
+        if (root->left == nullptr && root->right == nullptr) return 1;
+
+        if (root->left == nullptr) return find_height02(root->right) + 1;
+
+        if (root->right == nullptr) return find_height02(root->left) + 1;
+
+        int height = std::min(find_height02(root->left), find_height02(root->right)) + 1;
+
+        return height;
     }
 
-    bool isBalanced(TreeNode* root) {
-        return get_height(root) != -1;
+    int minDepth(TreeNode *root) {
+        return find_height02(root);
     }
 };
 
-int main110() {
+int main() {
     TreeNode *root = new TreeNode(3);
     root->left = new TreeNode(9);
     root->right = new TreeNode(20);
@@ -43,7 +47,7 @@ int main110() {
     root->right->right = new TreeNode(7);
 
     Solution sol;
-    std::cout << sol.isBalanced(root);
+    std::cout << sol.minDepth(root);
 
     return 0;
 }
