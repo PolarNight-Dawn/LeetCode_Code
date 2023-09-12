c852aec1e34a7e6759e3afdf41f39ce639f4ce5c
polarnight <1813959165@qq.com>
2023-09-12 14:52:26 +0800

    added 383_canConstruct.cpp's algorithm

--- 二周目/哈希表/242_isAnagram.cpp
+++ 二周目/哈希表/242_isAnagram.cpp
@@ -25,7 +25,7 @@ public:
     }
 };
 
-int main() {
+int main242() {
     std::string s = "anagram";
     std::string t = "nagaram";
 
---
+++ 二周目/哈希表/383_canConstruct.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+//
+// Created by polarnight on 23-9-12, 下午2:42.
+//
+
+class Solution {
+public:
+    bool canConstruct(std::string ransomNote, std::string magazine) {
+        if (ransomNote.size() > magazine.size()) return false;
+
+        std::vector<int> hash(26, 0);
+
+        for (auto &vec : magazine) {
+            hash[vec - 'a']++;
+        }
+
+        for (auto &vec : ransomNote) {
+            hash[vec - 'a']--;
+            if (hash[vec - 'a'] < 0) return false;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    std::string ransomNote = "a";
+    std::string magazine = "b";
+
+    Solution sol;
+    std::cout << sol.canConstruct(ransomNote, magazine);
+
+    return 0;
+}
