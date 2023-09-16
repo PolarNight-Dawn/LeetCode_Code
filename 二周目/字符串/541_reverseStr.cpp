68924c4f7c76a8ac21204fe65c3d5574ff0c67ca
polarnight <1813959165@qq.com>
2023-09-16 16:06:13 +0800

    modified 541_reverseStr.cpp's algorithm

--- 二周目/字符串/541_reverseStr.cpp
+++ 二周目/字符串/541_reverseStr.cpp
@@ -6,34 +6,44 @@
 
 class Solution {
 public:
-    std::string ReverseStr(std::string str) {
-        int len = str.size();
-        for (int left = 0, right = len - 1; left < right; left++, right--) {
+    void ReverseStr(std::string &str, int start, int end) {
+        for (int left = start, right = end; left < right; left++, right--) {
             std::swap(str[left], str[right]);
         }
-        return str;
     }
 
+    // std::string reverseStr(std::string s, int k) {
+    //     std::string str;
+    //     while(true) {
+    //         int len = s.size();
+    //         if (len < k) {
+    //             str += ReverseStr(s);
+    //             break;
+    //         }
+    //         if (len < 2 * k && len >= k) {
+    //             str += ReverseStr(s.substr(0, k));
+    //             str += s.substr(k);
+    //             break;
+    //         }
+    //
+    //         str += ReverseStr(s.substr(0, k));
+    //         str += s.substr(k, k);
+    //         s = s.substr(2 * k);
+    //     }
+    //
+    //     return str;
+    // }
+
+    // 优化代码 当需要一段一段去处理字符串的时候，可以想想在for循环的表达上做做文章
     std::string reverseStr(std::string s, int k) {
-        std::string str;
-        while(true) {
-            int len = s.size();
-            if (len < k) {
-                str += ReverseStr(s);
-                break;
+        for (int i = 0; i < s.size(); i += (2 * k)) {
+            if (i + k <= s.size()) {
+                ReverseStr(s, i, i + k - 1);
+                continue;
             }
-            if (len < 2 * k && len >= k) {
-                str += ReverseStr(s.substr(0, k));
-                str += s.substr(k);
-                break;
-            }
-
-            str += ReverseStr(s.substr(0, k));
-            str += s.substr(k, k);
-            s = s.substr(2 * k);
+            ReverseStr(s, i, s.size() - 1);
         }
-
-        return str;
+        return s;
     }
 };
 
