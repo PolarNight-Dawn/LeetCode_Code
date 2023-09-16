dadcb9d7eda1a7583f18ca65b9c0ba3537960a65
polarnight <1813959165@qq.com>
2023-09-16 15:14:01 +0800

    modified 344_reverseString.cpp's algorithm

--- 二周目/字符串/344_reverseString.cpp
+++ 二周目/字符串/344_reverseString.cpp
@@ -24,15 +24,21 @@ public:
 
     // 双指针法 时间复杂度为0(n)
     void reverseString(std::vector<char> &s) {
-        int left = 0;
-        int right = s.size() - 1;
+        // int left = 0;
+        // int right = s.size() - 1;
+        //
+        // while (left < right) {
+        //     char tmp = s[left];
+        //     s[left] = s[right];
+        //     s[right] = tmp;
+        //     left++;
+        //     right--;
+        // }
 
-        while (left < right) {
-            char tmp = s[left];
-            s[left] = s[right];
-            s[right] = tmp;
-            left++;
-            right--;
+        // 简化代码
+        // 另外强调对于库函数的使用 当你知道这个库函数的实现原理，并且库函数使用仅是解题的一部分，就可以
+        for (int left = 0, right = s.size() - 1; left < right; left++, right--) {
+            std::swap(s[left], s[right]);
         }
 
         return;
