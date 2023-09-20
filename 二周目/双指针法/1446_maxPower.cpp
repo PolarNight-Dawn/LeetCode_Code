0b3a6839c98fb1f54a770b006d971cd739bebd7d
polarnight <1813959165@qq.com>
2023-09-20 18:40:50 +0800

    modified 1446_maxPower.cpp's algorithm,the second round

--- 二周目/双指针法/1446_maxPower.cpp
+++ 二周目/双指针法/1446_maxPower.cpp
@@ -7,17 +7,34 @@
 class Solution {
 public:
     // 快慢双指针 代码太丑陋了
+    // int maxPower(std::string s) {
+    //     int s_len = s.size();
+    //     int len = 0;
+    //     for (int slow = 0, fast = 0; fast < s.size(); fast++) {
+    //         if (s[fast] != s[slow]) {
+    //             len = std::max(fast - slow, len);
+    //             slow = fast;
+    //         }
+    //         if (len == 0 && fast == s.size() - 1) len = s.size();
+    //         if (len != 0 && fast == s.size() - 1) len = std::max(fast - slow + 1, len);
+    //     }
+    //
+    //     return len;
+    // }
+
+    // 简化代码
     int maxPower(std::string s) {
         int s_len = s.size();
-        int len = 0;
+        int len = 0, cnt = 0;
         for (int slow = 0, fast = 0; fast < s.size(); fast++) {
             if (s[fast] != s[slow]) {
-                len = std::max(fast - slow, len);
+                len = std::max(len, cnt);
+                cnt = 0;
                 slow = fast;
             }
-            if (len == 0 && fast == s.size() - 1) len = s.size();
-            if (len != 0 && fast == s.size() - 1) len = std::max(fast - slow + 1, len);
+            cnt++;
         }
+        len = std::max(len, cnt);
 
         return len;
     }
