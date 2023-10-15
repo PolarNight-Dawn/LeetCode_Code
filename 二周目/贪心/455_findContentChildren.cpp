a7069be3350910a0cc31d71ade157d6f22d8ee8a
polarnight <1813959165@qq.com>
2023-10-15 17:53:42 +0800

    modified 455_findContentChildren.cpp's algorithm,the second round

--- 二周目/贪心/455_findContentChildren.cpp
+++ 二周目/贪心/455_findContentChildren.cpp
@@ -8,21 +8,31 @@
 
 class Solution {
 public:
-    int findContentChildren(std::vector<int>& g, std::vector<int>& s) {
+    int findContentChildren(std::vector<int> &g, std::vector<int> &s) {
         std::sort(g.begin(), g.end());
         std::sort(s.begin(), s.end());
 
-        int count = 0, index = 0;
-        for (int i = 0; i < g.size(); i++) {
-            for (int j = index; j < s.size(); j++) {
-                if (g[i] <= s[j]) {
-                    index = j + 1;
-                    count++;
-                    break;
-                }
-                if (g[i] > s[j] && j == s.size() - 1) return count;
+        //int count = 0, index = 0;
+        // for (int i = 0; i < g.size(); i++) {
+        //     for (int j = index; j < s.size(); j++) {
+        //         if (g[i] <= s[j]) {
+        //             index = j + 1;
+        //             count++;
+        //             break;
+        //         }
+        //         if (g[i] > s[j] && j == s.size() - 1) return count;
+        //     }
+        // }
+
+        // 简化代码，取消双循环的使用
+        int count = 0, index = s.size() - 1;
+        for (int i = g.size(); i >= 0; i--) {
+            if (index > 0 && g[i] <= s[index]) {
+                index--;
+                count++;
             }
         }
+
         return count;
     }
 };
