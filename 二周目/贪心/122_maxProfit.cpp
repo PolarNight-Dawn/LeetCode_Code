68a6d5923fbd9cb2d7f657088ccafbd569f1416a
polarnight <1813959165@qq.com>
2023-10-17 15:36:56 +0800

    modified 122_maxProfit.cpp's algorithm,the second round

--- 二周目/贪心/122_maxProfit.cpp
+++ 二周目/贪心/122_maxProfit.cpp
@@ -7,21 +7,31 @@
 
 class Solution {
 public:
+    // dp 时间复杂度O(n) 空间复杂度O(n)
+    // int maxProfit(std::vector<int>& prices) {
+    //     std::vector<std::vector<int>> dp(prices.size(), std::vector(2, 0));
+    //
+    //     for (int i = 0; i < prices.size(); i++) {
+    //         for (int j = 0; j < 2; j++) {
+    //             if (i == 0) {
+    //                 if (j == 0) dp[i][j] = 0;
+    //                 if (j == 1) dp[i][1] -= prices[0];
+    //             }
+    //             if (i > 0 && j == 0) dp[i][j] = std::max(dp[i - 1][j], dp[i - 1][j + 1] + prices[i]);
+    //             if (i > 0 && j == 1) dp[i][j] = std::max(dp[i - 1][j], dp[i - 1][j - 1] - prices[i]);
+    //         }
+    //     }
+    //
+    //     return std::max(dp[prices.size() - 1][0], dp[prices.size() - 1][1]);
+    // }
+
+    // 贪心 时间复杂度O(n) 空间复杂度O(1)
     int maxProfit(std::vector<int>& prices) {
-        std::vector<std::vector<int>> dp(prices.size(), std::vector(2, 0));
-
-        for (int i = 0; i < prices.size(); i++) {
-            for (int j = 0; j < 2; j++) {
-                if (i == 0) {
-                    if (j == 0) dp[i][j] = 0;
-                    if (j == 1) dp[i][1] -= prices[0];
-                }
-                if (i > 0 && j == 0) dp[i][j] = std::max(dp[i - 1][j], dp[i - 1][j + 1] + prices[i]);
-                if (i > 0 && j == 1) dp[i][j] = std::max(dp[i - 1][j], dp[i - 1][j - 1] - prices[i]);
-            }
+        int sum = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            sum += std::max(prices[i] - prices[i - 1], 0);
         }
-
-        return std::max(dp[prices.size() - 1][0], dp[prices.size() - 1][1]);
+        return sum;
     }
 };
 
