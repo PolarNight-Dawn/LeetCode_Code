6000a7ad54041379f6c5ca6b14268c8f8fcae446
polarnight <1813959165@qq.com>
2023-10-19 17:11:29 +0800

    modified 860_lemonadeChange.cpp's algorithm,the second round

--- 二周目/贪心/860_lemonadeChange.cpp
+++ 二周目/贪心/860_lemonadeChange.cpp
@@ -7,19 +7,35 @@
 
 class Solution {
 public:
-    bool lemonadeChange(std::vector<int>& bills) {
-        std::vector<int> balance(3, 0);
-        for (int i = 0;i < bills.size(); i++) {
-            if (i == 0 && bills[i] > 5) return false;
-            if (bills[i] == 5) balance[0] += 1;
-            else if (bills[i] == 10) balance[0] -= 1, balance[1] += 1;
-            else if (bills[i] == 20) {
-                if (balance[1] > 0)balance[0] -= 1, balance[1] -= 1, balance[2] += 1;
-                else if (balance[1] == 0) balance[0] -= 3, balance[2] += 1;
-            }
-            if (balance[0] < 0 || balance[1] < 0) return false;
-        }
+    // 时间复杂度为O(n) 空间复杂度为O(n)
+    // bool lemonadeChange(std::vector<int>& bills) {
+    //     std::vector<int> balance(3, 0);
+    //     for (int i = 0;i < bills.size(); i++) {
+    //         if (i == 0 && bills[i] > 5) return false;
+    //         if (bills[i] == 5) balance[0] += 1;
+    //         else if (bills[i] == 10) balance[0] -= 1, balance[1] += 1;
+    //         else if (bills[i] == 20) {
+    //             if (balance[1] > 0)balance[0] -= 1, balance[1] -= 1, balance[2] += 1;
+    //             else if (balance[1] == 0) balance[0] -= 3, balance[2] += 1;
+    //         }
+    //         if (balance[0] < 0 || balance[1] < 0) return false;
+    //     }
+    //
+    //     return true;
+    // }
 
+    // 优化代码 时间复杂度为O(n) 空间复杂度为O(1)
+    bool lemonadeChange(std::vector<int>& bills) {
+        int five = 0, ten = 0, twenty = 0;
+        for (auto &vec : bills) {
+            if (vec == 5) five++;
+            else if (vec == 10) five--, ten++;
+            else if (vec == 20) {
+                if (ten > 0) five--, ten--, twenty++;
+                else if (ten == 0) five -= 3, twenty++;
+            }
+            if (five < 0 || ten < 0) return false;
+        }
         return true;
     }
 };
