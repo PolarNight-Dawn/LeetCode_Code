d42865d04190e76bbcf9f5680a3f1b54361c3c28
polarnight <1813959165@qq.com>
2023-10-24 20:00:36 +0800

    modified 746_minCostClimbingStairs.cpp's algorithm,the second round

--- 二周目/动态规划/746_minCostClimbingStairs.cpp
+++ 二周目/动态规划/746_minCostClimbingStairs.cpp
@@ -9,12 +9,10 @@ class Solution {
 public:
     int minCostClimbingStairs(std::vector<int>& cost) {
         std::vector<int> dp(cost.size() + 1, 0);
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+        dp[0] = dp[1] = 0;
 
         for (int i = 2; i <= cost.size(); i++) {
-            dp[i] = std::min(dp[i - 1], dp[i - 2]) + cost[i];
-            if (i == cost.size()) dp[i] = std::min(dp[i - 1], dp[i - 2]);
+            dp[i] = std::min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
         return dp[cost.size()];
     }
