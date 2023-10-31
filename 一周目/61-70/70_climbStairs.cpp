b213515b5cf7637bcb6919b19fe34e8bc14158bb
polarnight <1813959165@qq.com>
2023-11-01 01:30:21 +0800

    added 70_climbStairs.cpp's algorithm,the second round

--- 一周目/61-70/70_climbStairs.cpp
+++ 一周目/61-70/70_climbStairs.cpp
@@ -7,20 +7,32 @@
 
 class Solution {
 public:
+    // int climbStairs(int n) {
+    //     std::vector<int> dp(n + 1, 0);
+    //     dp[0] = 1, dp[1] = 2;
+    //     if (n == 1 || n ==  2) return dp[n - 1];
+    //
+    //     for (int i = 2; i < n; i++) {
+    //         dp[i] = dp[i - 1] + dp[i - 2];
+    //     }
+    //
+    //     return  dp[n - 1];
+    // }
+
+    // 二周目 动态规划 完全背包问题
     int climbStairs(int n) {
         std::vector<int> dp(n + 1, 0);
-        dp[0] = 1, dp[1] = 2;
-        if (n == 1 || n ==  2) return dp[n - 1];
 
-        for (int i = 2; i < n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
-
-        return  dp[n - 1];
+        dp[0] = 1;
+        for (int i = 0; i <= n; i++)
+            for (int j = 1; j <= 2; j++)
+                if (i - j >= 0)
+                    dp[i] += dp[i - j];
+        return dp[n];
     }
 };
 
-int main70() {
+int main() {
     int n = 1;
 
     Solution sol;
--- 二周目/动态规划/279_numSquares.cpp
+++ 二周目/动态规划/279_numSquares.cpp
@@ -43,7 +43,7 @@ public:
     }
 };
 
-int main() {
+int main279() {
     int n = 12;
 
     Solution sol;
