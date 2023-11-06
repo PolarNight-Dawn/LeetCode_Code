81ac3b78c942fc02eda543d6a2301325b09d1133
polarnight <1813959165@qq.com>
2023-11-06 15:44:03 +0800

    added 123_maxProfit.cpp's algorithm,the second round

--- 二周目/动态规划/121_maxProfit.cpp
+++ 二周目/动态规划/121_maxProfit.cpp
@@ -12,7 +12,7 @@ public:
         int n = prices.size();
         std::vector<std::vector<int>> dp(n, std::vector(2, 0));
 
-        dp[0][0] = -7, dp[0][1] = 0;
+        dp[0][0] = -prices[0], dp[0][1] = 0;
         for (int i = 1; i < n; i++) {
             dp[i][0] = std::max(-prices[i], dp[i - 1][0]);
             dp[i][1] = std::max(dp[i - 1][0] + prices[i], dp[i - 1][1]);
@@ -21,7 +21,7 @@ public:
     }
 };
 
-int main() {
+int main121() {
     std::vector<int> prices = {7,1,5,3,6,4};
 
     Solution sol;
---
+++ 二周目/动态规划/123_maxProfit.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+//
+// Created by polarnight on 23-11-6, 下午2:58.
+//
+
+class Solution {
+public:
+    int maxProfit123(std::vector<int>& prices) {
+        int len = prices.size();
+        std::vector<std::vector<int>> dp(len, std::vector(5, 0));
+
+        dp[0][1] = -prices[0], dp[0][3] = -prices[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][1] = std::max(dp[i - 1][0] - prices[i], dp[i - 1][1]);
+            dp[i][2] = std::max(dp[i - 1][1] + prices[i], dp[i - 1][2]);
+            dp[i][3] = std::max(dp[i - 1][2] - prices[i], dp[i - 1][3]);
+            dp[i][4] = std::max(dp[i - 1][3] + prices[i], dp[i - 1][4]);
+        }
+        return dp[len - 1][4];
+    }
+};
+
+int main() {
+    std::vector<int> prices = {3,3,5,0,0,3,1,4};
+
+    Solution sol;
+    std::cout << sol.maxProfit123(prices);
+
+    return 0;
+}
