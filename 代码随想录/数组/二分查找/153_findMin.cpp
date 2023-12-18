e0f3de51f1c23adeea8501fa7f8e442872404d38
polarnight <1813959165@qq.com>
2023-12-18 17:43:39 +0800

    [随想录二刷]二分查找：152

---
+++ 代码随想录/数组/二分查找/153_findMin.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+//
+// Created by polarnight on 23-12-18, 下午5:10.
+//
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums.back()) left = mid + 1;
+            else right = mid - 1;
+        }
+        return nums[left];
+    }
+};
+
+int main() {
+    vector<int> nums = {3,1,2};
+
+    Solution sol;
+    cout << sol.findMin(nums);
+
+    return 0;
+}
