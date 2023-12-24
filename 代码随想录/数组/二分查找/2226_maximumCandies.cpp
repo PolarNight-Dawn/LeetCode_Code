70e91835e9495c2991957e8a6d72bbf2fd81a485
polarnight <1813959165@qq.com>
2023-12-24 17:32:32 +0800

    [每日一题]231224_1954, 231223_1962 [随想录二刷]二分查找：2226

--- 代码随想录/数组/二分查找/2226_maximumCandies.cpp
+++ 代码随想录/数组/二分查找/2226_maximumCandies.cpp
@@ -1,3 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 //
 // Created by polarnight on 23-12-24, 下午3:59.
 //
+
+class Solution {
+public:
+    int maximumCandies(vector<int>& candies, long long k) {
+        function<bool(int)> Check = [&](int i) -> bool {
+            long long ans = 0;
+            for (auto &candy : candies) ans += candy / i;
+            return ans >= k;
+        };
+
+        int max_val = *max_element(candies.begin(), candies.end());
+        int left = 1, right = max_val + 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (Check(mid)) left = mid + 1;
+            else right = mid - 1;
+        }
+        return right;
+    }
+};
+
+int main() {
+    vector<int> candies = {5,8,6};
+    int k = 3;
+
+    Solution sol;
+    cout << sol.maximumCandies(candies, k);
+
+    return 0;
+}
--- 每日一题/231224_1954_minimumPerimeter.cpp
+++ 每日一题/231224_1954_minimumPerimeter.cpp
@@ -1,3 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
 //
 // Created by polarnight on 23-12-24, 下午2:14.
 //
+
+class Solution {
+public:
+    //! \究极数学推导
+    // long long minimumPerimeter(long long neededApples) {
+    //      long long n = cbrt(neededApples / 4.0);
+    //
+    //      if (2 * n * (n + 1) * (2 * n + 1) < neededApples) n++;
+    //
+    //     return n * 8;
+    //  }
+
+    //! \二分法and数学
+    long long minimumPerimeter(long long neededApples) {
+        long long left = 1, right = 99999;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            if (2 * mid * (mid + 1) * (2 * mid + 1) >= neededApples) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left * 8;
+    }
+};
+
+int main231224() {
+    long long neededApples = 1000000000;
+
+    Solution sol;
+    cout << sol.minimumPerimeter(neededApples);
+
+    return 0;
+}
