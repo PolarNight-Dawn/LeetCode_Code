82cf2841426bd76d0a45ebbc1d9cd1463ab50a13
polarnight <1813959165@qq.com>
2024-01-01 16:40:15 +0800

    目录重构

---
+++ 随想录二刷/二分查找/二分答案/2258_maximumMinutes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+//
+// Created by polarnight on 24-1-1, 下午4:09.
+//
+
+class Solution {
+public:
+    int maximumMinutes(vector<vector<int>> &grid) {
+
+    }
+};
+
+int main() {
+    vector<vector<int>> grid = {{0, 2, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 2, 2, 1, 0},
+                                {0, 2, 0, 0, 1, 2, 0},
+                                {0, 0, 2, 2, 2, 0, 2},
+                                {0, 0, 0, 0, 0, 0, 0}};
+
+    Solution sol;
+    cout << sol.maximumMinutes(grid);
+
+    return 0;
+}
