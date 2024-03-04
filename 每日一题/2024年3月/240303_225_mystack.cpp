dcc75d6541ab27bd9957ffa2618c15553ab31c14
polarnight <1813959165@qq.com>
2024-03-04 14:26:00 +0800

    [重构目录 && 每日一题]240303_225

---
+++ 每日一题/2024年3月/240303_225_mystack.cpp
@@ -0,0 +1,48 @@
+//
+// Created by polarnight on 24-3-3, 下午2:33.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+class MyStack {
+public:
+    MyStack() {
+
+    }
+
+    void push(int x) {
+        que_in.push(x);
+    }
+
+    int pop() {
+        int size = static_cast<int>(que_in.size()) - 1;
+        while (size--) {
+            que_in.push(que_in.front());
+            que_in.pop();
+        }
+
+        int ret = que_in.front();
+        que_in.pop();
+        return ret;
+    }
+
+    int top() {
+        return que_in.back();
+    }
+
+    bool empty() {
+        return que_in.empty();
+    }
+
+private:
+    queue<int> que_in;
+};
+
+int main() {
+
+}
